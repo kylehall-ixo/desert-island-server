86d9579842dfda77f3391937336c0e61fff29e01
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-10 13:22:29 -0500

    fix incorrect content type

--- Ping/ping.js
+++ Ping/ping.js
@@ -2,13 +2,12 @@ const PingEndpoint = async function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
 
   context.res = {
-    headers: {
-      contentType: 'application/json'
-    },
     body: {
       msg: 'Pong! Plus some more to test the deployment works!'
     }
   }
+
+  context.res.headers['Content-Type'] = 'application/json'
 }
 
 module.exports = PingEndpoint

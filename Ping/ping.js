1c4af6b6e904699fcae528dc6ae9a66ee1a97c44
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-10 12:17:48 -0500

    add json content header

--- Ping/ping.js
+++ Ping/ping.js
@@ -2,6 +2,9 @@ const PingEndpoint = async function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
 
   context.res = {
+    headers: {
+      contentType: 'application/json'
+    },
     body: {
       msg: 'Pong! Plus some more to test the deployment works!'
     }
--- Ping/ping.spec.js
+++ Ping/ping.spec.js
@@ -12,4 +12,14 @@ describe('Ping', () => {
       msg: 'Pong! Plus some more to test the deployment works!'
     })
   })
+
+  it('should return json content type', () => {
+    const context = { log: () => {} }
+
+    ping(context)
+
+    expect(context.res.headers).to.deep.equal({
+      contentType: 'application/json'
+    })
+  })
 })
